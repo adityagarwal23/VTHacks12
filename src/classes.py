86d10e7a6bdc3aad3004cd84d31f171b0847be96
canTuncPekkan <pekkan.ct@gmail.com>
2024-09-14 11:20:29 -0400

    i added a text decoder to decode our data into classes

--- src/classes.py
+++ src/classes.py
@@ -6,12 +6,18 @@ class Course:
         self.number = number
         self.profesorList = profesorList
 
+    def __str__(self) -> str:
+        return self.subject + " " + self.number.__str__() + " : " + [i.__str__() for i in self.profesorList].__str__()
 
 
-class profesor:
+
+class Profesor:
     def __init__(self, firstName: str, lastName: str, difficulty: float) -> None:
         self.firstName = firstName
         self.lastName = lastName
-        self.difficulty = difficulty 
+        self.difficulty = difficulty
+
+    def __str__(self) -> str:
+        return self.firstName + " " + self.lastName + " Dif: " + self.difficulty.__str__()
 
 
